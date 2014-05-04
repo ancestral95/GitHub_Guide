97d5710db7fa54ed9cc46b1c364fba18890780cc
Davide <davide@informacomputer.it>
2014-05-04 14:35:50 +0200

    Delete carica_file.sh

--- carica_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# upload the file $1 in the online repo
-# in $2 there is the commit
-# You must run thi script in the local repo folder
-
-git add $1
-git commit -m "$2"
-git push -u origin master
