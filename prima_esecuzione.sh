90cfad28c284ab46d7525266e142b637ef98e043
Davide <davide@informacomputer.it>
2014-05-04 14:52:31 +0200

    first execution that create a repo

--- prima_esecuzione.sh
+++ prima_esecuzione.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # create a repo online
-# You must run thi script in the local repo folder
+# You must run this script in the local repo folder
 # in $1 there is the link to the repo online
+# in $2 there is the commit
 
 touch README.md
 git init
 git add README.md
-git commit -m "first commit"
+git commit -m "$2"
 git remote add origin $1
 git push -u origin master
